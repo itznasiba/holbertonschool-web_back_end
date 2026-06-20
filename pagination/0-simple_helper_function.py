05489dfd1ea4cbe48ba3f9709d38f3e80e8483e2
itznasiba <zeynallinesibe2006@gmail.com>
2026-06-20 18:17:32 +0000

    1

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+python function that returns tuple size
+"""
+
+
+def index_range(page:int, page_size:int)->tuple[int, int]:
+    """
+    Calculate the start and end indexes for a given page and page size.
+
+    Args:
+        page (int): The 1-indexed page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple[int, int]: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
