cbd3ddf442f03155f2809b7c1e23ee924e483717
itznasiba <zeynallinesibe2006@gmail.com>
2026-07-12 19:50:38 +0000

    1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -6,9 +6,9 @@
  * @returns {Array<number>} An array of student IDs, or an empty array if the input is invalid.
  */
 
-export default function getListStudentId(stduents) {
-	if(!Array.isArray(students){
-		return [];
-	}
-	return students.map((student)=>student.id);
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
 }
---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Filters a list of students by a specific location.
+ * @param {Array} students - The array of student objects.
+ * @param {string} city - The city to filter by.
+ * @returns {Array<Object>} An array of students located in the specified city.
+ */
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
+}
